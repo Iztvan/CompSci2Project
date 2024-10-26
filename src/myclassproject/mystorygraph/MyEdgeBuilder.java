2851426fecfb2d7580c4c0e221aec651a79c438b
Iztvan <138512071+Iztvan@users.noreply.github.com>
2024-10-26 18:15:31 -0500

    changed my color theme and added frst node

--- src/myclassproject/mystorygraph/MyEdgeBuilder.java
+++ src/myclassproject/mystorygraph/MyEdgeBuilder.java
@@ -11,6 +11,8 @@ public class MyEdgeBuilder extends NodeBuilder {
 	 * Initializes the list of story graph nodes.
 	 * @param list A list of all story graph nodes.
 	 */
+
+	 
 	public MyEdgeBuilder(List<Node> list) {
 		super(list);
 	}
@@ -21,8 +23,21 @@ public class MyEdgeBuilder extends NodeBuilder {
 	 * The method should add the edges of the node one by one. 
 	 * These methods must have a BuilderMethod annotation.
 	 */
-	@Override
+	//Istvan
+	@BuilderMethod
+	public void rootEdges() {
+		var root = get(NodeLabels.root.toString());
+		var choice = new MenuChoice(MenuChoice.Options.Start);
+		var nextNode = get(NodeLabels.atCottage.toString());
+		root.add(new Edge(choice, nextNode));
+	}
 	
+	//Istvan
+	@BuilderMethod
+	public void introEdges() {
+		var intro = get(NodeLabels.intro.toString());
+		var 
+	}
 	@BuilderMethod
 	public void rootEdges() {
 		//Example:
