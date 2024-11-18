339804493f3cc7cc647603916f9bd647bf5b31e4
Iztvan <138512071+Iztvan@users.noreply.github.com>
2024-11-17 20:07:41 -0600

    Fixed capitalization in Story Entities

--- src/myclassproject/main/MainClass.java
+++ src/myclassproject/main/MainClass.java
@@ -1,6 +1,7 @@
 package myclassproject.main;
 
 import com.storygraph.Story;
+import myclassproject.mystorygraph.MyGraph;
 
 import myclassproject.questexample.QuestGraph;
 import myclassproject.testgraph.TestGraph;
@@ -11,7 +12,7 @@ public class MainClass {
 		//Create an object of your story class.
 		var story = new Story();
 		//Simply call run.
-		story.Run(new QuestGraph());
+		story.Run(new MyGraph());
 	}
 
 }
